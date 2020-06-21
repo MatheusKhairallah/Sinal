96ab904b1b20311b9a3cec78fb66b13f6a0268fb
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-06-20 21:17:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Sinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escreva um número: ");
+            decimal numero=Convert.ToDecimal(Console.ReadLine());
+
+            Console.Clear();
+
+            if(numero>0)
+            Console.WriteLine("positivo");
+            else if(numero<0)
+            Console.WriteLine("negativo");
+            else
+            Console.WriteLine("zero");
+
+
+            
+            
+        }
+    }
+}
